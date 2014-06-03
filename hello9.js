95d16ca59e8b280a148302e115d4634b2ed9cc97
shigemk2 <i.am.shige@gmail.com>
2014-06-04 00:07:05 +0900

    P83

---
+++ hello9.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs   = require('fs');
+var url  = require('url');
+var ejs  = require('ejs');
+
+var index = fs.readFileSync('./index.ejs', 'utf8');
+var style = fs.readFileSync('./style.css', 'utf8');
+
+var server = http.createServer();
+server.on('request', doRequest);
+server.listen(1337);
+
+function doRequest(req, res) {
+    var path = url.parse(req.url);
+
+    switch(path.pathname) {
+    case '/':
+        var ck = req.headers.cookie;
+        var tmp = ejs.render(index, {
+            title: "Index Page",
+            msg:"クッキー: " + ck
+        });
+        res.setHeader('Content-Type','text/html');
+        res.write(tmp);
+        res.end();
+        break;
+    case '/style.css': // スタイルシート用
+        res.setHeader('Content-Type','text/css');
+        res.write(style);
+        res.end();
+        break;
+    default:
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Set-Cookie', ['lasturl=' + path.pathname]);
+        res.write('SET COOKIE!');
+        res.end();
+        break;
+    }
+}
+
+console.log('Server running at http://127.0.0.1:1337/');
+
+
+
