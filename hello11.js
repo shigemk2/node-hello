2ba7f80786cb2ebe4dae0d02d6a047cac1f4c432
shigemk2 <i.am.shige@gmail.com>
2014-06-05 00:50:15 +0900

    P99

---
+++ hello11.js
@@ -0,0 +1,44 @@
+var http = require('http');
+var fs   = require('fs');
+var url  = require('url');
+var jade  = require('jade');
+
+var index = fs.readFileSync('./index.jade', 'utf8');
+var style = fs.readFileSync('./style.css', 'utf8');
+
+var server = http.createServer();
+server.on('request', doRequest);
+server.listen(1337);
+
+function doRequest(req, res) {
+    var path = url.parse(req.url);
+
+    switch(path.pathname) {
+    case '/':
+        var fn = jade.compile(index);
+        // compileで返された関数を実行することでレンダリングされたHTMLコードが生成される。
+        var tmp = fn();
+
+        res.setHeader('Content-Type','text/html');
+        res.write(tmp);
+        res.end();
+        break;
+    case '/style.css': // スタイルシート用
+        res.setHeader('Content-Type','text/css');
+        res.write(style);
+        res.end();
+        break;
+    case '/favicon.ico':
+        break;
+    default:
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('NO PAGE!');
+        res.end();
+        break;
+    }
+}
+
+console.log('Server running at http://127.0.0.1:1337/');
+
+
+
